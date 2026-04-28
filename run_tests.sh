1b5da18547bdce3d7372ee60d29154d850efc42c
Andrew <andrew.zhang3@student.unimelb.edu.au>
2026-04-28 21:36:12 +1200

    Added exe file extension to test script for gitignore purposes

--- run_tests.sh
+++ run_tests.sh
@@ -6,6 +6,6 @@ javac Java_implementation/*.java
 echo -e "\n=== Java Fork Test Results ==="
 java -cp . Java_implementation.Game_of_life_fork "tests_configs.csv"
 echo -e "\n=== Compiling C++ Fork (O3 + OpenMP) ==="
-g++ -O3 -fopenmp Cpp_implementation/Cpp_fork.cpp -o Cpp_implementation/Cpp_fork
+g++ -O3 -fopenmp Cpp_implementation/Cpp_fork.cpp -o Cpp_implementation/Cpp_fork.exe
 echo -e "\n=== C++ Fork Test Results ==="
-./Cpp_implementation/Cpp_fork tests_configs.csv
+./Cpp_implementation/Cpp_fork.exe tests_configs.csv
